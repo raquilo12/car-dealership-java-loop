707443160be798774100d025025f98bf78a3500a
raquilo <raquilo@vbox.lan>
2025-06-10 19:58:07 -0500

    Initial commit with Java restaurant app

---
+++ RestaurantInfo.java
@@ -0,0 +1,14 @@
+
+public class RestaurantInfo {
+    public static void main(String[] args) {
+        // Declare and initialize variables
+        String restaurantName = "King Buffet";
+        String streetAddress = "10250 Technology Blvd W";
+        String cityStateZip = "Dallas, TX 75220";
+
+        // Print the restaurant information
+        System.out.println(restaurantName);
+        System.out.println(streetAddress);
+        System.out.println(cityStateZip);
+    }
+}
